c9972bcdab328746d592d5e11c1a2b9882810c38
Darien Lozon <dnl0009@mail.wvu.edu>
2018-11-26 17:25:12 -0500

    Bring it together

--- land_notes.R
+++ land_notes.R
@@ -31,25 +31,29 @@ for (k in 1:length(myFiles)) {
   ldf[[k]] <- read.dbf(myFiles[k])
 } # this brings all files in as one object with k values
 # Separate files.. I bet I can loop this somehow... 
-Farm1L <- ldf[[1]]
-Farm2L <- ldf[[2]]
-Farm3L <- ldf[[3]]
-FPR1L <- ldf[[4]]
-FPR2L <- ldf[[5]]
-FPR4L <- ldf[[7]]
-FPR5L <- ldf[[8]]
-FPR6L <- ldf[[9]]
-FPR7L <- ldf[[10]]
-FPR8L <- ldf[[11]]
-FPR9L <- ldf[[12]]
-HPL <- ldf[[13]]
-JesseL <- ldf[[14]]
-Keener1L <- ldf[[15]]
-LaurelL <- ldf[[16]]
-RRL <- ldf[[19]]
-UDC1L <- ldf[[20]]
-UDC2L <- ldf[[21]]
-WPL <- ldf[[22]]
+Born1 <- ldf[[1]]
+Farm1L <- ldf[[2]]
+Farm2L <- ldf[[3]]
+Farm3L <- ldf[[4]]
+Farm4L <- ldf[[5]]
+FPR1L <- ldf[[6]]
+FPR2L <- ldf[[7]]
+FPR4L <- ldf[[9]]
+FPR5L <- ldf[[10]]
+FPR6L <- ldf[[11]]
+FPR7L <- ldf[[12]]
+FPR8L <- ldf[[13]]
+FPR9L <- ldf[[14]]
+HPL <- ldf[[15]]
+JesseL <- ldf[[16]]
+Keener1L <- ldf[[17]]
+Keener2L <- ldf[[18]]
+LaurelL <- ldf[[19]]
+ReservoirL <- ldf[[20]]
+RRL <- ldf[[23]]
+UDC1L <- ldf[[24]]
+UDC2L <- ldf[[25]]
+WPL <- ldf[[26]]
 
 ####################################
 ## I'll combine the data with similar lengths.
@@ -65,9 +69,9 @@ PLUS12
 
 PLUS1 <- merge(PLUS13,PLUS12,by="VALUE",all=TRUE)
 
-PLUS9 <- cbind(Farm1L[,c(1,4)],Keener1L[,4],LaurelL[,4])
-colnames(PLUS9) <- c("VALUE","Farm1","Keener1","Laurel")
-PLUS9[,2:4] <- round(PLUS9[,2:4],3) # Don't forget the ,3; otherwise it'll round to the nearest 1
+PLUS9 <- cbind(Farm1L[,c(1,4)],Keener1L[,4],LaurelL[,4],Keener2L[,4],Born1[,4])
+colnames(PLUS9) <- c("VALUE","Farm1","Keener1","Laurel","Keener2","Born1")
+PLUS9[,2:6] <- round(PLUS9[,2:6],3) # Don't forget the ,3; otherwise it'll round to the nearest 1
 PLUS9
 
 PLUS1 <- merge(PLUS1,PLUS9,by="VALUE",all=TRUE); PLUS1 # Don't forget the ALL=TRUE
@@ -80,26 +84,55 @@ PLUS10
 
 PLUS1 <- merge(PLUS1,PLUS10,by="VALUE",all=T); PLUS1
 
-PLUS1 <- PLUS1[,c(1,4,5,3,6,7,8,9,20,10,12,15,11,2,21,14,16,18,19,17,13)]; PLUS1
-PLUS <- merge(PLUS1,Farm3L[,c(1,4)],by="VALUE",all=T) # Poor little lone wolf
-PLUS[,22] <- round(PLUS[,22],3)
-colnames(PLUS) <- c("VALUE","FPR1","FPR2","FPR3","FPR4","FPR5","FPR6","FPR7","FPR8","FPR9","UDC1","UDC2","RR","RP","Jesse","Hodges","Wolfe","Keener1","Laurel","Farm1","Farm2","Farm3")
-PLUS <- PLUS[,2:22] # Don't need the value column anymore 
+PLUS8 <- cbind(Farm3L[,c(1,4)],Farm4L[,4],ReservoirL[,4])
+colnames(PLUS8) <- c("VALUE","Farm3","Farm4","Reservoir")
+PLUS8[,2:4] <- round(PLUS8[,2:4],3)
+PLUS8
 
-####################################################################
+PLUS1 <- merge(PLUS1,PLUS8,by="VALUE",all=T); PLUS1
+PLUS <- PLUS1[,c(1,4,5,3,6,7,8,9,22,10,12,15,11,2,26,23,14,16,21,18,20,19,17,13,24,25)]; PLUS
+colnames(PLUS) <- c("VALUE","FPR1","FPR2","FPR3","FPR4","FPR5","FPR6","FPR7","FPR8","FPR9","UDC1","UDC2","RR","RP","Reservoir","Jesse","Hodges","Wolfe","Born1","Keener1","Keener2","Laurel","Farm1","Farm2","Farm3","Farm4")
+rownames(PLUS) <- c("Forested","Grass/Past/Ag","Barren/Dev","OpenWater",
+                    "MineGrass","MineBarren","SMCRAForest","PreSMCRAGrass",
+                    "PreSMCRABarren","PreSMCRAForested","HerbaceousWetland",
+                    "WoodyWetland","Road")
+PLUS <- PLUS[,2:26] # Don't need the value column anymore 
+
+###################################################################
 ###################################################################
 #################### MOVING ON ####################################
 ####################  TO THE   ####################################
-#################### GRAPHING  #####################################
+#################### GRAPHING  ####################################
 ###################################################################
 ###################################################################
 
+install.packages("colorRamps") # Make sure package is in quotes
+library(colorRamps) # Call package
 PLUS <- as.matrix(PLUS) # Need to get rid of the NA values I worked so hard to get -- HA!
 PLUS[is.na(PLUS)] <- 0; PLUS # Because the bars cannot be drawn with NA values
-plot1 <- barplot(prop.table(PLUS,2),main="Landscape Mosaics within 1 km of Each Site",font.main=4,xlab="Site",ylab="Proportion of Hectares (%)",col=colorRamps::primary.colors(13),las=2,cex.names = 0.8,font.lab=2)
-legend("bottomleft",legend=c("Forested","Grass/Past/Ag","Barren/Dev",
-                             "OpenWater","MineGrass","MineBarren",
-                             "SMCRAForest","PreSMCRAGrass","PreSMCRABarren",
-                             "PreSMCRAForested","HerbaceousWetland",
-                             "WoodyWetland","Road"),cex=0.5, inset= c(0.175,0.01), fill=colorRamps::primary.colors(13))
+
+plot1 <- barplot(prop.table(PLUS,2),main="Landscape Mosaics within 1 km of Each Site",
+                 font.main=4,xlab="Site",ylab="Proportion of Hectares (%)",
+                 col=colorRamps::primary.colors(13),las=2,cex.names = 0.8,font.lab=2)
+legend("bottomleft",bg="white",legend=c("Forested","Grass/Past/Ag","Barren/Dev",
+                                        "OpenWater","MineGrass","MineBarren",
+                                        "SMCRAForest","PreSMCRAGrass","PreSMCRABarren",
+                                        "PreSMCRAForested","HerbaceousWetland",
+                                        "WoodyWetland","Road"),cex=0.8, inset= c(0.08,0.02), fill=colorRamps::primary.colors(13))
+
 ## EXACTEMENT! :D
+## cex = 0.5, inset = c(0.175,0.01) for laptop 
+## I think I have different versions on my different computers.
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+
+survey <- PLUS[,c(1:3,12:14)] # Pick out sites that have been surveyed for presab comparison
+t(survey) # Flip data frame for adding presab column (for both CP and CS) 
+## T stands for TRANSPOSE, which means "to trade places"
+
+
